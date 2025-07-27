09d1364c7fec3e962532de2f31f1501f90bb41e9
Ibuki.O <isan.ethen@gmail.com>
2025-07-27 21:14:01 +0900

    fix: Use uds_stream fd instead of chan fd.

--- src/fdtbl.rs
+++ src/fdtbl.rs
@@ -7,16 +7,29 @@ fn from_syscall_error(error: SyscallError) -> io::Error {
     io::Error::from_raw_os_error(error.errno as i32)
 }
 
-fn create_dummy_fd(name: &str) -> io::Result<usize> {
-    let path = format!("/scheme/chan/{}", name);
-    syscall::open(path, syscall::O_CREAT | syscall::O_RDWR).map_err(from_syscall_error)
-}
+const RESOURCE_NAME: &str = "/scheme/uds_stream";
+fn prepare_fd_to_send(name: &str) -> io::Result<usize> {
+    let fd = syscall::open(RESOURCE_NAME, syscall::O_RDWR | syscall::O_CREAT)
+        .map_err(from_syscall_error)?;
 
-fn verify_fpath(fd: usize, expected_path_prefix: &str, expected_name: &str) -> io::Result<()> {
+    let mut name_str = name.to_string();
+
+    let payload = unsafe { name_str.as_bytes_mut() };
+    redox_rt::sys::sys_call(
+        fd,
+        payload,
+        CallFlags::empty(),
+        &[redox_rt::protocol::SocketCall::Bind as u64],
+    )
+    .map_err(from_syscall_error)?;
+
+    Ok(fd)
+}
+fn verify_fpath(fd: usize, expected_name: &str) -> io::Result<()> {
     let mut buffer = [0u8; 128];
     let bytes_read = syscall::fpath(fd, &mut buffer).map_err(from_syscall_error)?;
     let path_str = std::str::from_utf8(&buffer[..bytes_read]).unwrap();
-    let expected_path = format!("{}/{}", expected_path_prefix, expected_name);
+    let expected_path = format!("{}/{}", RESOURCE_NAME, expected_name);
     println!("      fpath({}) -> '{}'", fd, path_str);
     assert_eq!(path_str, expected_path);
     Ok(())
@@ -79,29 +92,31 @@ fn receive_fds(
 }
 
 pub fn run_all() -> anyhow::Result<()> {
-    let scheme_path = "/scheme/chan";
     println!("\n--- FdTbl Indirect Tests ---");
 
     println!("[TEST] Automatic allocation to POSIX table and sends with clone and should failed with EBADF");
     let (receiver, sender) = create_socket_pair()?;
-    let fd1 = create_dummy_fd("test_posix_auto1")?;
-    let fd2 = create_dummy_fd("test_posix_auto2")?;
+    let fd1 = prepare_fd_to_send("test_posix_auto1")?;
+    let fd2 = prepare_fd_to_send("test_posix_auto2")?;
+    println!("Sending FDs with clone and automatic allocation to POSIX table");
     send_fds_with_clone(sender, &[fd1, fd2]).map_err(from_syscall_error)?;
 
     let mut new_fds_posix = [0_usize; 2];
     receive_fds(receiver, &mut new_fds_posix, CallFlags::empty())?;
     println!("   -> Received FDs: {:?}", new_fds_posix);
-    verify_fpath(new_fds_posix[0], scheme_path, "test_posix_auto1")?;
-    verify_fpath(new_fds_posix[1], scheme_path, "test_posix_auto2")?;
+    verify_fpath(new_fds_posix[0], "test_posix_auto1")?;
+    verify_fpath(new_fds_posix[1], "test_posix_auto2")?;
 
+    println!("Sending FDs with move)");
     send_fds(sender, &[fd1, fd2]).map_err(from_syscall_error)?;
 
     let mut new_fds_posix = [0_usize; 2];
     receive_fds(receiver, &mut new_fds_posix, CallFlags::empty())?;
     println!("   -> Received FDs: {:?}", new_fds_posix);
-    verify_fpath(new_fds_posix[0], scheme_path, "test_posix_auto1")?;
-    verify_fpath(new_fds_posix[1], scheme_path, "test_posix_auto2")?;
+    verify_fpath(new_fds_posix[0], "test_posix_auto1")?;
+    verify_fpath(new_fds_posix[1], "test_posix_auto2")?;
 
+    println!("Sending moved FDs (should fail with EBADF)");
     let result = send_fds(sender, &[fd1, fd2]);
     assert!(result.is_err(), "Expected an error but got Ok");
     if let Err(e) = result {
@@ -110,8 +125,8 @@ pub fn run_all() -> anyhow::Result<()> {
     }
 
     println!("[TEST] Automatic allocation to upper table");
-    let fd3 = create_dummy_fd("test_upper_auto1")?;
-    let fd4 = create_dummy_fd("test_upper_auto2")?;
+    let fd3 = prepare_fd_to_send("test_upper_auto1")?;
+    let fd4 = prepare_fd_to_send("test_upper_auto2")?;
     send_fds(sender, &[fd3, fd4]).map_err(from_syscall_error)?;
 
     let mut new_fds_upper = [0_usize; 2];
@@ -119,23 +134,23 @@ pub fn run_all() -> anyhow::Result<()> {
     println!("   -> Received FDs: {:?}", new_fds_upper);
     assert_eq!(new_fds_upper[0] & UPPER_FDTBL_TAG, UPPER_FDTBL_TAG);
     assert_eq!(new_fds_upper[1] & UPPER_FDTBL_TAG, UPPER_FDTBL_TAG);
-    verify_fpath(new_fds_upper[0], scheme_path, "test_upper_auto1")?;
-    verify_fpath(new_fds_upper[1], scheme_path, "test_upper_auto2")?;
+    verify_fpath(new_fds_upper[0], "test_upper_auto1")?;
+    verify_fpath(new_fds_upper[1], "test_upper_auto2")?;
 
     println!("[TEST] Manual allocation to upper table");
-    let fd5 = create_dummy_fd("test_upper_manual1")?;
-    let fd6 = create_dummy_fd("test_upper_manual2")?;
+    let fd5 = prepare_fd_to_send("test_upper_manual1")?;
+    let fd6 = prepare_fd_to_send("test_upper_manual2")?;
     send_fds(sender, &[fd5, fd6]).map_err(from_syscall_error)?;
 
     let mut manual_fds = [100 | UPPER_FDTBL_TAG, 200 | UPPER_FDTBL_TAG];
     receive_fds(receiver, &mut manual_fds, CallFlags::FD_UPPER)?;
     println!("   -> Received FDs into slots: {:?}", manual_fds);
-    verify_fpath(manual_fds[0], scheme_path, "test_upper_manual1")?;
-    verify_fpath(manual_fds[1], scheme_path, "test_upper_manual2")?;
+    verify_fpath(manual_fds[0], "test_upper_manual1")?;
+    verify_fpath(manual_fds[1], "test_upper_manual2")?;
 
     println!("[TEST] Manual allocation to upper table with invalid slots range (should fail with EMFILE)");
-    let fd5 = create_dummy_fd("test_upper_manual1")?;
-    let fd6 = create_dummy_fd("test_upper_manual2")?;
+    let fd5 = prepare_fd_to_send("test_upper_manual1")?;
+    let fd6 = prepare_fd_to_send("test_upper_manual2")?;
     send_fds(sender, &[fd5, fd6]).map_err(from_syscall_error)?;
 
     let mut manual_fds = [100 | UPPER_FDTBL_TAG, (65_536 + 1) | UPPER_FDTBL_TAG];
@@ -149,11 +164,11 @@ pub fn run_all() -> anyhow::Result<()> {
     println!("[TEST] Manual upper allocation to an occupied slot (should fail with EEXIST)");
     let (receiver, sender) = create_socket_pair()?;
 
-    let obstacle_fd = create_dummy_fd("obstacle")?;
+    let obstacle_fd = prepare_fd_to_send("obstacle")?;
     syscall::dup2(obstacle_fd, 150 | UPPER_FDTBL_TAG, &[])?;
 
-    let failing_fd1 = create_dummy_fd("should_fail1")?;
-    let failing_fd2 = create_dummy_fd("should_fail2")?;
+    let failing_fd1 = prepare_fd_to_send("should_fail1")?;
+    let failing_fd2 = prepare_fd_to_send("should_fail2")?;
     send_fds(sender, &[failing_fd1, failing_fd2]).map_err(from_syscall_error)?;
 
     let mut failing_slot = [50 | UPPER_FDTBL_TAG, 150 | UPPER_FDTBL_TAG];
