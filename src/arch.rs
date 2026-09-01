b3daa73b2b788cc666fba4104fefe72d014718bc
4lDO2 <4lDO2@protonmail.com>
2026-09-01 17:32:00 +0200

    Add basic support for performance counters.

--- src/arch.rs
+++ src/arch.rs
@@ -3,3 +3,14 @@ mod x86_64;
 
 #[cfg(target_arch = "x86_64")]
 pub use x86_64::*;
+
+#[cfg(not(target_arch = "x86_64"))]
+pub struct PerfCtrState;
+
+#[cfg(not(target_arch = "x86_64"))]
+impl PerfCtrState {
+    pub fn new() -> Option<Self> {
+        // TODO: print warning?
+        None
+    }
+}
